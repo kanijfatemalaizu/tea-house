f405271bd22dd54611f1b2085458badeda66ed05
kanijfatemalaizu <kanij.fatema.laizu@gmail.com>
2024-03-06 22:53:11 -0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       light: {
+  //         ...require("daisyui/src/theming/themes")["light"],
+  //         ".btn-twitter": {
+  //           "background-color": "#1EA1F1",
+  //           "border-color": "#1EA1F1",
+  //         },
+  //         ".btn-twitter:hover": {
+  //           "background-color": "#1C96E1",
+  //           "border-color": "#1C96E1",
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
+};
